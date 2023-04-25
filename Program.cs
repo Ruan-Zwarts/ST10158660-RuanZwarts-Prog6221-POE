7821d2e20453b3be5839850c4444762fbc5a54ee
RuanZwarts <ST10158660@vcconnect.edu.za>
2023-04-25 23:15:54 +0200

    Adding comments

--- Program.cs
+++ Program.cs
@@ -88,30 +88,30 @@ namespace ST10158660_RuanZwarts_Prog6221_POE
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)     //Main method to put everything together
         {
             NewRecipe recipe = new NewRecipe();
 
             while (true)
             {
-                Console.WriteLine("Enter the number of ingredients:");
+                Console.WriteLine("Enter the number of ingredients:");      //entering the number of ingredients
                 int numIngredients = Convert.ToInt32(Console.ReadLine());
 
                 for (int i = 0; i < numIngredients; i++)
                 {
-                    Console.WriteLine("Enter the name of ingredient {0}:", i + 1);
+                    Console.WriteLine("Enter the name of ingredient {0}:", i + 1);      //entering the name of the ingredient, eg Eggs/Flour/Milk,etc
                     string name = Console.ReadLine();
 
-                    Console.WriteLine("Enter the quantity of ingredient {0}:", i + 1);
+                    Console.WriteLine("Enter the quantity of ingredient {0}:", i + 1);  //entering the amount of this ingredient
                     double quantity = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter the unit of measurement of ingredient {0}:", i + 1);
+                    Console.WriteLine("Enter the unit of measurement of ingredient {0}:", i + 1);   //giving the unit of measurement of the ingredient above
                     string unitOfMeasure = Console.ReadLine();
 
                     recipe.AddIngredient(new IngredientList(name, quantity, unitOfMeasure));
                 }
 
-                Console.WriteLine("Enter the number of steps:");
+                Console.WriteLine("Enter the number of steps:");            //entering the amount of steps needed to make the recipe
                 int numSteps = Convert.ToInt32(Console.ReadLine());
 
                 for (int i = 0; i < numSteps; i++)
@@ -128,19 +128,19 @@ namespace ST10158660_RuanZwarts_Prog6221_POE
                     Console.WriteLine("Enter '1' to scale the recipe, '2' to reset quantities, '3' to clear recipe, or press anything else to exit:");
                     string input = Console.ReadLine();
 
-                    if (input == "1")
+                    if (input == "1")       //gives the user the ability to scale the order
                     {
                         Console.WriteLine("Enter the scaling factor (0.5, 2, or 3):");
                         double factor = Convert.ToDouble(Console.ReadLine());
                         recipe.ScaleRecipe(factor);
                         recipe.DisplayRecipe();
                     }
-                    else if (input == "2")
+                    else if (input == "2")  //gives the user the ability to reset the recipe quantities
                     {
                         recipe.Reset();
                         recipe.DisplayRecipe();
                     }
-                    else if (input == "3")
+                    else if (input == "3")  //gives the user the ability to clear the recipe
                     {
                         recipe.ClearRecipe();
                         break;
